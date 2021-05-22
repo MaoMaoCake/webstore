2f7537318fb5e73d00927b7755b9fda0da235d09
Jannyney <jan.tawanrat@gmail.com>
2021-05-22 15:04:09 +0700

    lay

--- app.py
+++ app.py
@@ -1,43 +1,53 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-    return 'Hello World!'
+    return render_template("layout.html")
+
 
 @app.route("/register")
 def register():
     pass
 
+
 @app.route("/login")
 def login():
     pass
+
 
 @app.route("/logout")
 def logout():
     pass
 
+
 @app.route("/changepw")
 def change_password():
     pass
+
 
 @app.route("/addmoney")
 def add_money():
     pass
 
+
 @app.route("/purchase")
-def show_products():
+def purchase():
     pass
+
 
 @app.route("/products")
 def show_products():
     pass
 
+
 @app.route("/cart")
-def show_products():
+def show_cart():
     pass
+
 
 if __name__ == '__main__':
     app.run()
